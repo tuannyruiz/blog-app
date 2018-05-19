37cb121dffa90954d659d68c2fcc6798004010ca
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-05-19 13:53:26 +0000

    create index template

--- app.js
+++ app.js
@@ -2,3 +2,36 @@ var bodyParser = require("body-parser"),
     express = require("express"),
     app = express(),
     mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+
+app.set("view engine", "ejs");
+app.use(express.static("public")); //servir o aplicativo com uma folha de estilos customizada
+app.use(bodyParser.urlencoded({extended: true}));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req, res) {
+   res.redirect("/blogs"); 
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err) {
+            console.log("Error!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("Server is running!") 
+});
